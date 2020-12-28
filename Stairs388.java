e2e7e0d2374a4545fd7fd6533b2fc0fed7aeb712
Yosuke Yamaguchi <yonosho456@gmail.com>
2020-12-28 23:13:27 +0900

    ２回標準入力を受け付けるように修正。問題の意図はこっちだった。

--- Stairs388.java
+++ Stairs388.java
@@ -1,25 +1,16 @@
 import java.util.Scanner;
-import java.util.Arrays;
-import java.util.List;
 
 // https://yukicoder.me/problems/no/289
 public class Stairs388 {
-
-
   public static void main(String[] args) {
-    // 標準入力から読み込む際に、Scannerオブジェクトを使う。
-    Scanner sc = new Scanner(System.in);
+    // 標準入力から読み込む際に、Scan
 
     // String １つ分を読み込む
-    String s = sc.next();
+    int s1 = Integer.parseInt(sc.next());
+    int s2 = Integer.parseInt(sc.next());
 
-    // String s = "39 10";
 
-    List<String> inputList = Arrays.asList(s.split(" "));
-    int stairNumber = Integer.parseInt(inputList.get(0));
-    int floorNumber = Integer.parseInt(inputList.get(1));
-
-    int result = stairNumber / floorNumber;
+    int result = s1 / s2;
     // 標準出力に書き出す。
     System.out.println(result + 1);
   }

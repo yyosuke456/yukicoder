c556acb5f19f05c47383b6e68a6f56cf15b1aea2
Yosuke Yamaguchi <yonosho456@gmail.com>
2020-12-29 22:54:42 +0900

    ポッキーゲームの問題を解くプログラムを追加

--- Pockey63.java
+++ Pockey63.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-// https://yukicoder.me/problems/no/289
-public class Stairs388 {
+// https://yukicoder.me/problems/no/63
+public class Pockey63 {
   public static void main(String[] args) {
     // 標準入力から読み込む際に、Scan
     Scanner sc = new Scanner(System.in);
@@ -9,9 +9,14 @@ public class Stairs388 {
     // String １つ分を読み込む
     int s1 = Integer.parseInt(sc.next());
     int s2 = Integer.parseInt(sc.next());
-
-    int result = s1 / s2;
+    int result = 0;
+    while (s1 > s2 * 2) {
+      s1 -= s2 * 2;
+      if (s1 >= 0) {
+        result += s2;
+      }
+    }
     // 標準出力に書き出す。
-    System.out.println(result + 1);
+    System.out.println(result);
   }
 }

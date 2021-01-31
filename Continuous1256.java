9e4d80cc55470f3716b7d6ab99bffb30a8c0d4d1
Yosuke Yamaguchi <yonosho456@gmail.com>
2021-01-31 20:41:07 +0900

    ３の倍数かどうかで判定

---
+++ Continuous1256.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+// https://yukicoder.me/problems/no/1256
+public class Continuous1256 {
+  public static void main(String[] args) {
+    // 標準入力から読み込む際に、Scan
+    Scanner sc = new Scanner(System.in);
+
+    // String １つ分を読み込む
+    int n = Integer.parseInt(sc.next());
+    if (n % 3 == 0) {
+      System.out.println("YES");
+    } else {
+      System.out.println("NO");
+    }
+  }
+}

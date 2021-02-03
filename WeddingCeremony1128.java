504ad961b7587ebac6f0c557e7545834843c09c5
Yosuke Yamaguchi <yonosho456@gmail.com>
2021-02-03 23:35:15 +0900

    2で割り切れる場合は1を足す

---
+++ WeddingCeremony1128.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+// https://yukicoder.me/problems/no/1128
+public class WeddingCeremony1128 {
+  public static void main(String[] args) {
+    // 標準入力から読み込む際に、Scan
+    Scanner sc = new Scanner(System.in);
+
+    // String １つ分を読み込む
+    int n = Integer.parseInt(sc.next());
+    if (n % 2 == 0) {
+      System.out.println(n);
+    } else {
+      System.out.println(n + 1);
+    }
+  }
+}
